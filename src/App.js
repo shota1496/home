df665b4b555d51d2a2a2094f25a53565626284f6
Shota-Ishikuro <shukatu2012@gmail.com>
2024-09-22 00:08:06 +0900

    add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo_1" alt="logo" />
+        <img src={logo} className="App-logo_2" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
