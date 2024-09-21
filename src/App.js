4090ac07c0489e59b45ef512dcf70a200aff4795
Shota-Ishikuro <shukatu2012@gmail.com>
2024-09-22 01:11:53 +0900

    編集

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from "./logo.svg";
+import maria from "./maria.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <hr />
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo_2" alt="logo" />
+          <img src={logo} className="App-logo_1" alt="logo" />
+          <img src={logo} className="App-logo_2" alt="logo" />
+          <img src={logo} className="App-logo_1" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <hr />
+        <h1>指出毬亜の差し出がましいようですが</h1>
+        <img src={maria} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={logo} className="App-logo_1" alt="logo" />
-        <img src={logo} className="App-logo_2" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
